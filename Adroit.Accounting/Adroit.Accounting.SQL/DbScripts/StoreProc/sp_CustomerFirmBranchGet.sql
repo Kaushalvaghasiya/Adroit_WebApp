fa50f8306565d7f8b116e1f8cb731397a7465214
jaimin patel <jaimin@imriel.com>
2023-05-19 00:39:28 +0530

    data field add validation number only

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_CustomerFirmBranchGet.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_CustomerFirmBranchGet.sql
@@ -4,7 +4,8 @@ CREATE OR ALTER PROCEDURE [dbo].[sp_CustomerFirmBranchGet]
 )
 AS
 BEGIN
-	SELECT *
+	SELECT *, CONVERT(VARCHAR(10), CustomerFirmBranch.RenewalDate, 101) as RenewalDateStr,
+	   CONVERT(VARCHAR(10), CustomerFirmBranch.AddedOn, 101) as AddedOnStr
 	FROM CustomerFirmBranch WHERE Id = @Id
 END
 GO
