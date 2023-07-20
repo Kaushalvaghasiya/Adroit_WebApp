b1fd0230c36245b567c50e033033cbb443cd3789
Vishal <vishal.simon@hotmail.com>
2023-07-20 10:25:13 +0530

    V/SP changes

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0012 140-Customer.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0012 140-Customer.sql
@@ -1,6 +1,9 @@
 ALTER TABLE [SystemSetting] add [NewRegistrationSoftwarePlanId] TINYINT NOT NULL DEFAULT(1)
 GO
 
+DECLARE @ID TINYINT
 INSERT INTO [FirmBranchTypeAdmin] (Title, OrderNumber, IsDeleted, Active) VALUES ('Head Office', 0, 0, 1)
-UPDATE [SystemSetting] SET NewRegistrationFirmBranchTypeId = 0
+SET @ID = SCOPE_IDENTITY()
+
+UPDATE [SystemSetting] SET NewRegistrationFirmBranchTypeId = @ID 
 GO
