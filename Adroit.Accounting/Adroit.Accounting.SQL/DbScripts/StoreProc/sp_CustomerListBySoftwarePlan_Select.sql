0a2991d2886820ba6876cfe5bf5a8521b0d7a392
Vishal <vishal.simon@hotmail.com>
2023-07-31 10:39:29 +0530

    V/Menusetting bug

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_CustomerListBySoftwarePlan_Select.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_CustomerListBySoftwarePlan_Select.sql
@@ -7,9 +7,11 @@ BEGIN
 	SELECT DISTINCT
 		Customer.Id As Value, Customer.[Name] + ' (' + Customer.BusinessName + ')' As Text
 	FROM Software 
+	INNER JOIN SoftwarePlan ON Software.Id = SoftwarePlan.SoftwareId
 	INNER JOIN CustomerFirm ON CustomerFirm.SoftwareId = Software.Id
+	INNER JOIN CustomerFirmBranch ON CustomerFirm.Id = CustomerFirmBranch.FirmId
 	INNER JOIN Customer ON CustomerFirm.CustomerId = Customer.Id
-	WHERE Software.Id = @Id
+	WHERE CustomerFirmBranch.SoftwarePlanId = @Id 
 	AND Customer.IsDeleted = 0
 	AND CustomerFirm.IsDeleted = 0
 	AND Software.IsDeleted = 0
