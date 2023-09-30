227a4b431ef97bf72efd672c3a35da684f87b2d1
jaimin <jaimin.patel@allata.com>
2023-09-30 23:28:16 +0530

    District data UI

--- Adroit.Accounting/Adroit.Accounting.Model/Admin/District.cs
+++ Adroit.Accounting/Adroit.Accounting.Model/Admin/District.cs
@@ -13,5 +13,8 @@ namespace Adroit.Accounting.Model
         public string Title { get; set; }
         public int StateId { get; set; }
         public bool Active { get; set; }
+        public string Description { get; set; }
+        public string StateName { get; set; }
+        public string CountryName { get; set; }
     }
 }
