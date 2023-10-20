92fc53551ebc8f50faecf484822cf7b764c85b3b
jashmin patel <zasminpatel@gmail.com>
2023-10-20 10:42:47 +0530

    conflict resolved comit,

--- Adroit.Accounting/Adroit.Accounting.Web2/Controllers/CustomerController.cs
+++ Adroit.Accounting/Adroit.Accounting.Web2/Controllers/CustomerController.cs
@@ -23,7 +23,6 @@ namespace Adroit.Accounting.Web.Controllers
         protected readonly ConfigurationData _configurationData;
         private readonly ICommon _commonRepository;
         private readonly ITransportDesc _transportDescRepository;
-        public CustomerController(ICustomerAccount customerAccountRepo,
         public CustomerController(
             IVehicle vehicleRepo,
             IVehicleModel vehicleModelRepository,
