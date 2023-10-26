840caad6fd8b3e1c21423cbbe6473c0d67e288e9
Kartik J Chudasam <kartik@skylineinfosys.com>
2023-10-26 18:16:00 +0530

    TASK 81 (Adroit >> Account >> Books) Completed.

---
+++ Adroit.Accounting/Adroit.Accounting.Model/ViewModel/BookAdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+//    Copyright (c) Adroit IBS Pvt Ltd. All rights reserved.
+//-----------------------------------------------------------------------
+
+namespace Adroit.Accounting.Model.ViewModel
+{
+    public class BookAdminViewModel : BookAdmin
+    {
+        public List<DropdownViewModel> BookAccountList { get; set; }
+        public List<DropdownViewModel> BookTypeList { get; set; }
+        public List<DropdownViewModel> BillTypeList { get; set; }
+        public List<DropdownViewModel> BillFromList { get; set; }
+    }
+}
