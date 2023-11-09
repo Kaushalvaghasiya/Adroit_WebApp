93aca08c41e960d34c9908caa840500ff8605496
Kartik J Chudasama <kartik@skylineinfosys.com>
2023-11-09 10:58:07 +0530

    Name is unique.. So remove group id from constraint before that removed duplicate name rows. build new script 0007 AccountAdmin.sql

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0007 AccountAdmin.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0007 AccountAdmin.sql
@@ -1,7 +1,15 @@
+;WITH DeleteDuplicateAccountAdmin AS (
+    SELECT Name,
+           ROW_NUMBER() OVER (PARTITION BY Name ORDER BY (SELECT NULL)) AS RowNum
+    FROM [dbo].[AccountAdmin]
+)
+DELETE FROM DeleteDuplicateAccountAdmin
+WHERE RowNum > 1;
+GO
+
 ALTER TABLE [dbo].[AccountAdmin] ADD CONSTRAINT
-		    [IX_AccountAdmin1] UNIQUE NONCLUSTERED 
+		    [IX_AccountAdmin] UNIQUE NONCLUSTERED 
 		    (
-				[AccountGroupId] ASC,
 		    	[Name]  ASC
 		    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
