530013581d1bad384a3dcf038f6df2e737c6f1e4
Kartik Chudasama <Kartik@Skylineinfosys.com>
2023-12-15 18:13:33 +0530

    Added Not exists in Drop constrant on 0017 SalesBillMaster.sql

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0017 SalesBillMaster.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0017 SalesBillMaster.sql
@@ -1,7 +1,13 @@
+IF EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_Z-SalesBillmaster-Z]') AND parent_object_id = OBJECT_ID(N'[dbo].[Z-SalesBillMaster-Z]'))
+BEGIN
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP CONSTRAINT [CK_Z-SalesBillmaster-Z];
+END
 GO 
 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF_Z-SalesBillmaster-Z_InvoiceMemo]') AND type = 'D')
+BEGIN
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP CONSTRAINT [DF_Z-SalesBillmaster-Z_InvoiceMemo];
+END
 GO 
 
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP COLUMN [InvoiceMemo];
