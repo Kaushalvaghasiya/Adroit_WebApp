f145ec65cb616e62ae7826774fbd59e3298fcaa9
Kartik Chudasama <Kartik@Skylineinfosys.com>
2023-12-15 17:12:38 +0530

    Server issue fixed.

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0017 SalesBillMaster.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/Tables/0017 SalesBillMaster.sql
@@ -1,6 +1,11 @@
+ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP CONSTRAINT [CK_Z-SalesBillmaster-Z];
+GO 
+
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP CONSTRAINT [DF_Z-SalesBillmaster-Z_InvoiceMemo];
+GO 
 
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] DROP COLUMN [InvoiceMemo];
+GO
 
 ALTER TABLE [dbo].[Z-SalesBillMaster-Z] ADD [InvoiceMemo] [bit] NOT NULL;
 GO
