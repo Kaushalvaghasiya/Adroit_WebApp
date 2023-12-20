563719b60b0025c62f452971f08cc1fdc693fc79
Kartik Chudasama <Kartik@Skylineinfosys.com>
2023-12-20 10:18:28 +0530

    Fixed Issue in LR Booking, AddedCustomer Account edit inputs as a partial view.

--- Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_LRBookingGet.sql
+++ Adroit.Accounting/Adroit.Accounting.SQL/DbScripts/StoreProc/sp_LRBookingGet.sql
@@ -7,20 +7,23 @@ CREATE OR ALTER PROCEDURE [dbo].[sp_LRBookingGet]
 )
 AS
 BEGIN
-
 	DECLARE @CustomerId INT = dbo.fn_GetCustomerId(@LoginId);
 
 	SELECT [Z-LRBooking-Z].*
 	,[TransportDesc].Title As Description
 	,[TransportPacking].Title As Packing
+	,(SELECT TOP 1 LRDate
+	  FROM [Z-LRBooking-Z]
+	  WHERE [Z-LRBooking-Z].Id < @Id AND [Z-LRBooking-Z].BranchId = @BranchId 
+	  ORDER BY [Z-LRBooking-Z].Id DESC) AS LRBookingMaxDate,
+	 (SELECT TOP 1 LRDate
+	  FROM [Z-LRBooking-Z]
+	  WHERE [Z-LRBooking-Z].Id > @Id AND [Z-LRBooking-Z].BranchId = @BranchId 
+	  ORDER BY [Z-LRBooking-Z].Id ASC) AS LRBookingMinDate
 	FROM [Z-LRBooking-Z]
 	LEFT JOIN [TransportDesc] on [TransportDesc].Id = [Z-LRBooking-Z].DescriptionId AND [TransportDesc].CustomerId = @CustomerId AND [TransportDesc].Deleted = 0 AND [TransportDesc].Active = 1
 	LEFT JOIN [TransportPacking] on [TransportPacking].Id = [Z-LRBooking-Z].PackingId AND [TransportPacking].CustomerId = @CustomerId AND [TransportPacking].Deleted = 0 AND [TransportPacking].Active = 1
 	WHERE [Z-LRBooking-Z].Id = @Id AND [Z-LRBooking-Z].BranchId = @BranchId
 
-	SELECT ISNULL(CONVERT(VARCHAR(10), MIN(LRDate), 103), CONVERT(VARCHAR(10), GETDATE(), 103)) As LRBOOKINGMINDATE 
-	FROM [Z-LRBooking-Z]
-	WHERE [Z-LRBooking-Z].BranchId = @BranchId
-	
 END
 GO
