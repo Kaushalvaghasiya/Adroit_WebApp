3dfb1abd7db29dfa99d6a4ecb90d7443c542050f
zasminpatel <zasminpatel@gmail.com>
2023-12-26 09:59:30 +0530

    Book & Register>>LR Delivery Register>>Stock Register
    initail file commit.

---
+++ Adroit.Accounting/Adroit.Accounting.Model/ReportViewModel/LRDeliveryStockRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Adroit.Accounting.Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adroit.Accounting.Model.ReportViewModel
+{
+    public partial class LRDeliveryStockRegisterViewModel
+    {
+        public int[] BranchId { get; set; }
+        public List<DropdownViewModel> BranchList { get; set; }
+        public int[] CityFromId { get; set; }
+        public int[] CityToId { get; set; }
+        public List<DropdownViewModel> CityList { get; set; }
+        public int[] ConsignorId { get; set; }
+        public List<DropdownViewModel> ConsignorList { get; set; }
+        public int[] ConsigneeId { get; set; }
+        public List<DropdownViewModel> ConsigneeList { get; set; }
+        public int[] PayTypeId { get; set; }
+        public List<DropdownViewModel> PayTypeList { get; set; }
+        public int[] ChalanId { get; set; }
+        public List<DropdownViewModel> ChalanList { get; set; }
+        public int InvStatus { get; set; }
+        public List<DropdownViewModel> InvStatusList { get; set; }
+        public bool Summary { get; set; }
+    }
+}
---
+++ Adroit.Accounting/Adroit.Accounting.Web2/Controllers/ReportsLRDeliveryStockRegisterController.cs
@@ -0,0 +1,41 @@
+﻿using Adroit.Accounting.Model;
+using Adroit.Accounting.Model.Master;
+using Adroit.Accounting.Model.ReportViewModel;
+using Adroit.Accounting.Repository;
+using Adroit.Accounting.Repository.IRepository;
+using Adroit.Accounting.SQL.Tables;
+using Adroit.Accounting.Utility;
+using Adroit.Accounting.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adroit.Accounting.Web.Controllers
+{
+    public partial class CustomerReportController : MasterController
+    {
+        public IActionResult StockRegister()
+        {
+            LRDeliveryStockRegisterViewModel model = new LRDeliveryStockRegisterViewModel();
+            model.BranchList = _customerFirmBranchRepository.SelectListWithFirmByFirmId(CurrentFirmId, _configurationData.DefaultConnection);
+
+            model.CityList = _transportLRBranchCityMappingRepository.SelectList(_configurationData.DefaultConnection, CurrentBranchId);
+            model.ConsignorList = _customerAccountBranchMapping.GetCustomerAccountBranchMappingList(CurrentFirmId, CurrentBranchId, _configurationData.DefaultConnection);
+            model.ConsigneeList = model.ConsignorList;
+            model.PayTypeList = _transportLRPayTypeRepository.SelectList(_configurationData.DefaultConnection);
+
+            model.InvStatusList = new List<Model.ViewModel.DropdownViewModel> {
+                new Model.ViewModel.DropdownViewModel { Value = "0", Text ="Static Chhe"},
+                new Model.ViewModel.DropdownViewModel { Value = "1", Text = "All"},
+                new Model.ViewModel.DropdownViewModel { Value = "2", Text = "Invoice Done"},
+                new Model.ViewModel.DropdownViewModel { Value = "3", Text = "Pending Invoice"},
+                new Model.ViewModel.DropdownViewModel { Value = "4", Text = "Forward Chalan"}
+            };
+            model.ChalanList = new List<Model.ViewModel.DropdownViewModel> {
+                new Model.ViewModel.DropdownViewModel { Value = "0", Text ="Static Chhe"},
+                new Model.ViewModel.DropdownViewModel { Value = "1", Text = "All"},
+                new Model.ViewModel.DropdownViewModel { Value = "2", Text = "Chalan Done"},
+                new Model.ViewModel.DropdownViewModel { Value = "3", Text = "Pending Chalan"}
+            };
+            return View(model);
+        }
+    }
+}
