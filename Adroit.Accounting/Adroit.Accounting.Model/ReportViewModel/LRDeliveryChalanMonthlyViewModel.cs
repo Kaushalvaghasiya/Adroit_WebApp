950cd1e19adba614e46245b40007c397bcf053fe
zasminpatel <zasminpatel@gmail.com>
2023-12-27 17:50:54 +0530

    192 Reports>>Book & Register>>LR Delivery Register>>ChalanMonthlyReport
    192
    Reports>>Book & Register>>LR Delivery Register>>ChalanMonthlyReport
    done

---
+++ Adroit.Accounting/Adroit.Accounting.Model/ReportViewModel/LRDeliveryChalanMonthlyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Adroit.Accounting.Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adroit.Accounting.Model.ReportViewModel
+{
+    public partial class LRDeliveryChalanMonthlyViewModel
+    {
+        public int[] BranchId { get; set; }
+        public List<DropdownViewModel> BranchList { get; set; }
+    }
+}
