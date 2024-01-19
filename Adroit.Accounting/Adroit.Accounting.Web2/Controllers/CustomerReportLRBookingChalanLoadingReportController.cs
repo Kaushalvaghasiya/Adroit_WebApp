11cbf1cb5d489fe330e93908f617267d74d5ae5b
zasminpatel <zasminpatel@gmail.com>
2024-01-19 16:57:31 +0530

    task 184
    initial file creattion and related changes

--- Adroit.Accounting/Adroit.Accounting.Web2/Controllers/CustomerReportLRBookingChalanLoadingReportController.cs
+++ Adroit.Accounting/Adroit.Accounting.Web2/Controllers/CustomerReportLRBookingChalanLoadingReportController.cs
@@ -49,6 +49,105 @@ namespace Adroit.Accounting.Web.Controllers
             }
             return Json(result);
         }
+        [Route("~/CustomerReport/LRBookingChalanLoadingReportListWithSummary")]
+        public JsonResult LRBookingChalanLoadingReportListWithSummary(LRBookingChalanLoadingReportViewModel model, int draw = 0, int start = 0, int length = 10)
+        {
+            var result = new DataTableListViewModel<LRBookingChalanLoadingGridViewModel>();
+            try
+            {
+                //save to session
+                HttpContext.Session.SetString("LRBookingChalanLoadingReportListWithSummary", JsonConvert.SerializeObject(model));
+
+                var search = Request.Query["search[value]"];
+                var sortColumn = int.Parse(Request.Query["order[0][column]"]);
+                var sortDirection = Request.Query["order[0][dir]"];
+
+                var records = _reportLRBookingChalanLoadingReportRepository.GetListWithSummary(model, _configurationData.DefaultConnection, CurrentUserId, CurrentFirmId, search, start, length, sortColumn, sortDirection).ToList();
+                result.data = records;
+                result.recordsTotal = records.Count > 0 ? records[0].TotalCount : 0;
+                result.recordsFiltered = records.Count > 0 ? records[0].TotalCount : 0;
+            }
+            catch (Exception ex)
+            {
+                result.data = new List<LRBookingChalanLoadingGridViewModel>();
+                result.recordsTotal = 0;
+                result.recordsFiltered = 0;
+            }
+            return Json(result);
+        }
+        public IActionResult LRBookingChalanLoadingPrintDateWise()
+        {
+            var result = new ReportDataViewModel<List<LRBookingChalanLoadingGridViewModel>>();
+            result.ReportHeader = new ReportHeaderViewModel();
+            result.ReportHeader.Subtotal = bool.TryParse(HttpContext.Request.Query["SubTotal"], out bool subtotalValue) ? subtotalValue : false;
+            result.ReportHeader.DateFrom = DateTime.Parse(HttpContext.Request.Query["DateFrom"].ToString());
+            result.ReportHeader.DateTo = DateTime.Parse(HttpContext.Request.Query["DateTo"].ToString());
+            var parameters = JsonConvert.DeserializeObject<LRBookingChalanLoadingGridViewModel>(HttpContext.Session.GetString("LRBookingChalanLoadingReportList"));
+
+            result.ReportData = _reportLRBookingChalanLoadingReportRepository.GetList(parameters, _configurationData.DefaultConnection, CurrentUserId, CurrentFirmId, pageSize: -1).ToList();
+            var currentFirm = _customerFirmRepository.Get(CurrentFirmId, CurrentFirmId, _configurationData.DefaultConnection);
+            var currentBranch = _customerFirmBranchRepository.Get(CurrentBranchId, CurrentFirmId, _configurationData.DefaultConnection);
+            result.ReportHeader.FrimName = currentFirm.Title.ToString();
+            result.ReportHeader.BranchName = currentBranch.Title.ToString();
+            result.ReportHeader.BranchAddress = currentBranch.Address1.ToString();
+
+            return View(result);
+        }
+        public IActionResult LRBookingChalanLoadingPrintDateWiseSummary()
+        {
+            var result = new ReportDataViewModel<List<LRBookingChalanLoadingGridViewModel>>();
+            result.ReportHeader = new ReportHeaderViewModel();
+            result.ReportHeader.Subtotal = bool.TryParse(HttpContext.Request.Query["SubTotal"], out bool subtotalValue) ? subtotalValue : false;
+            result.ReportHeader.DateFrom = DateTime.Parse(HttpContext.Request.Query["DateFrom"].ToString());
+            result.ReportHeader.DateTo = DateTime.Parse(HttpContext.Request.Query["DateTo"].ToString());
+            var parameters = JsonConvert.DeserializeObject<LRBookingChalanLoadingGridViewModel>(HttpContext.Session.GetString("LRBookingChalanLoadingReportListWithSummary"));
+
+            result.ReportData = _reportLRBookingChalanLoadingReportRepository.GetListWithSummary(parameters, _configurationData.DefaultConnection, CurrentUserId, CurrentFirmId, pageSize: -1).ToList();
+            var currentFirm = _customerFirmRepository.Get(CurrentFirmId, CurrentFirmId, _configurationData.DefaultConnection);
+            var currentBranch = _customerFirmBranchRepository.Get(CurrentBranchId, CurrentFirmId, _configurationData.DefaultConnection);
+            result.ReportHeader.FrimName = currentFirm.Title.ToString();
+            result.ReportHeader.BranchName = currentBranch.Title.ToString();
+            result.ReportHeader.BranchAddress = currentBranch.Address1.ToString();
+
+            return View(result);
+        }
+        public IActionResult LRBookingChalanLoadingPrintTruckWise()
+        {
+            var result = new ReportDataViewModel<List<LRBookingChalanLoadingGridViewModel>>();
+            result.ReportHeader = new ReportHeaderViewModel();
+            result.ReportHeader.Subtotal = bool.TryParse(HttpContext.Request.Query["SubTotal"], out bool subtotalValue) ? subtotalValue : false;
+            result.ReportHeader.DateFrom = DateTime.Parse(HttpContext.Request.Query["DateFrom"].ToString());
+            result.ReportHeader.DateTo = DateTime.Parse(HttpContext.Request.Query["DateTo"].ToString());
+            var parameters = JsonConvert.DeserializeObject<LRBookingChalanLoadingGridViewModel>(HttpContext.Session.GetString("LRBookingChalanLoadingReportList"));
+
+            result.ReportData = _reportLRBookingChalanLoadingReportRepository.GetList(parameters, _configurationData.DefaultConnection, CurrentUserId, CurrentFirmId, pageSize: -1).ToList();
+            var currentFirm = _customerFirmRepository.Get(CurrentFirmId, CurrentFirmId, _configurationData.DefaultConnection);
+            var currentBranch = _customerFirmBranchRepository.Get(CurrentBranchId, CurrentFirmId, _configurationData.DefaultConnection);
+            result.ReportHeader.FrimName = currentFirm.Title.ToString();
+            result.ReportHeader.BranchName = currentBranch.Title.ToString();
+            result.ReportHeader.BranchAddress = currentBranch.Address1.ToString();
+
+            return View(result);
+        }
+        public IActionResult LRBookingChalanLoadingPrintTruckWiseSummary()
+        {
+            var result = new ReportDataViewModel<List<LRBookingChalanLoadingGridViewModel>>();
+            result.ReportHeader = new ReportHeaderViewModel();
+            result.ReportHeader.Subtotal = bool.TryParse(HttpContext.Request.Query["SubTotal"], out bool subtotalValue) ? subtotalValue : false;
+            result.ReportHeader.DateFrom = DateTime.Parse(HttpContext.Request.Query["DateFrom"].ToString());
+            result.ReportHeader.DateTo = DateTime.Parse(HttpContext.Request.Query["DateTo"].ToString());
+            var parameters = JsonConvert.DeserializeObject<LRBookingChalanLoadingGridViewModel>(HttpContext.Session.GetString("LRBookingChalanLoadingReportListWithSummary"));
+
+            result.ReportData = _reportLRBookingChalanLoadingReportRepository.GetListWithSummary(parameters, _configurationData.DefaultConnection, CurrentUserId, CurrentFirmId, pageSize: -1).ToList();
+            var currentFirm = _customerFirmRepository.Get(CurrentFirmId, CurrentFirmId, _configurationData.DefaultConnection);
+            var currentBranch = _customerFirmBranchRepository.Get(CurrentBranchId, CurrentFirmId, _configurationData.DefaultConnection);
+            result.ReportHeader.FrimName = currentFirm.Title.ToString();
+            result.ReportHeader.BranchName = currentBranch.Title.ToString();
+            result.ReportHeader.BranchAddress = currentBranch.Address1.ToString();
+
+            return View(result);
+        }
+
         //[HttpGet]
         //public JsonResult LRBookingChalanLoadingReportList1(int draw = 0, int start = 0, int length = 10)
         //{
