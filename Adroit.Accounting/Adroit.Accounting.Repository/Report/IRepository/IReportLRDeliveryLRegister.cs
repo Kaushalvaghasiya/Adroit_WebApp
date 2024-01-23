75ca6c5f0030587c82da87b461fb2e7ee2fb7671
Kaushalvaghasiya <kaushal.vaghasiya.2408@gmail.com>
2024-01-23 16:27:37 +0530

    LR Delivery LR Register Updated, Check Condtions for VehicleNumberId mapping and ChalanId mapping

---
+++ Adroit.Accounting/Adroit.Accounting.Repository/Report/IRepository/IReportLRDeliveryLRegister.cs
@@ -0,0 +1,12 @@
+﻿using Adroit.Accounting.Model.GridViewModel;
+using Adroit.Accounting.Model.ViewModel;
+using Adroit.Accounting.Model.ReportViewModel;
+
+namespace Adroit.Accounting.Repository
+{
+    public interface IReportLRDeliveryLRRegister
+    {
+        public List<LRDeliveryLRRegisterGridViewModel> GetList(LRDeliveryLRRegisterViewModel value, string connectionString, int loginId, int firmId = 0, string search = "", int pageStart = 0, int pageSize = 10, int sortColumn = 0, string sortOrder = "ASC");
+        public List<LRDeliveryLRRegisterGridViewModel> GetListWithSummary(LRDeliveryLRRegisterViewModel value, string connectionString, int loginId, int firmId = 0, string search = "", int pageStart = 0, int pageSize = 10, int sortColumn = 0, string sortOrder = "ASC");
+    }
+}
