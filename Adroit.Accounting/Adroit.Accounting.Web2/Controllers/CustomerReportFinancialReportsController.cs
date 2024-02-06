ca07a51d5e037badf2a96b33294d82e9d7b1f52c
Kaushalvaghasiya <kaushal.vaghasiya.2408@gmail.com>
2024-02-06 09:58:19 +0530

    ui updated --creditnote -sales,purchaseinterest --debitnote -purchase,salesInterest ui created --gstr01,ledger,all financial reports

---
+++ Adroit.Accounting/Adroit.Accounting.Web2/Controllers/CustomerReportFinancialReportsController.cs
@@ -0,0 +1,38 @@
+﻿using Adroit.Accounting.Model;
+using Adroit.Accounting.Model.GridViewModel;
+using Adroit.Accounting.Model.ReportViewModel;
+using Adroit.Accounting.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace Adroit.Accounting.Web.Controllers
+{
+    public partial class CustomerReportController : MasterController
+    {        
+        public IActionResult ProfitLoss()
+        {
+            return View();
+        }
+        public IActionResult TradingAccount()
+        {
+            return View();
+        }
+        public IActionResult BalanceSheet()
+        {
+            return View();
+        }
+        public IActionResult BalanceSheetSchedule()
+        {
+            return View();
+        }
+        public IActionResult TrialBalanceSummary()
+        {
+            return View();
+        }
+        public IActionResult TrialBalanceDetail()
+        {
+            return View();
+        }
+    }
+}
